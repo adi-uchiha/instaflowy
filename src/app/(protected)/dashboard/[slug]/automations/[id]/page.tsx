1983c7ce1289e4139710560bb17a0d3fe1cf5f4f
adi-uchiha <adi.cs.night@gmail.com>
2025-05-05 13:56:32 +0530

    Add automation list cards

---
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+	return (
+		<div className='flex flex-col items-center'>Page</div>
+	)
+}
+
+export default Page
--- src/components/global/automation-list/index.tsx
+++ src/components/global/automation-list/index.tsx
@@ -3,6 +3,8 @@ import { usePath } from '@/hooks/user-nav'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import React from 'react'
+import GradientButton from '../gradient-button'
+import { Button } from '@/components/ui/button'
 
 type Props = {}
 
@@ -22,14 +24,51 @@ const AutomationList = (props: Props) => {
 						This is from the comment
 					</p>
 					<div className='flex gap-x-2 flex-wrap mt-3'>
-					<div className={cn(
-						'rounded-full px-4 py-1 capitalize',
-						(0 + 1) % 1 ==0 && 
-						'bg-[#1e7f2086] border-1 border-green-500'
-					)}>
-						getstarted
+						<div className={cn(
+							'rounded-full px-4 py-1 capitalize',
+							(0 + 1) % 1 == 0 &&
+							'bg-[#1e7f2086] border-1 border-green-500'
+						)}>
+							getstarted
+						</div>
+						<div className={cn(
+							'rounded-full px-4 py-1 capitalize',
+							(0 + 1) % 1 == 0 &&
+							'bg-purple-950 border-1 border-purple-500'
+						)}>
+							getstarted
+						</div>
+						<div className={cn(
+							'rounded-full px-4 py-1 capitalize',
+							(0 + 1) % 1 == 0 &&
+							'bg-yellow-950 border-1 border-yellow-500'
+						)}>
+							getstarted
+						</div>
+						<div className={cn(
+							'rounded-full px-4 py-1 capitalize',
+							(0 + 1) % 1 == 0 &&
+							'bg-red-950 border-1 border-red-500'
+						)}>
+							getstarted
+						</div>
 					</div>
+					<div className='rounded-full border-2 mt-3 border-dashed border-white/60 px-3 py-1'>
+						<p className='text-sm text-[#bfc0c3]'>
+							No Keywords
+						</p>
 					</div>
+				</div>
+				<div className='flex flex-col justify-between'>
+					<p className='capitalize text-sm font-light text-[#9b9ca0]'>
+						October 5th 2025
+					</p>
+					<GradientButton type="BUTTON" className='w-full text-white hover:bg-gray-900 bg-gray-700' >
+						Smart AI
+					</GradientButton>
+					<Button className='bg-gray-600 hover:bg-gray-700'>
+						Standard
+					</Button>
 				</div>
 			</Link>
 		</div>
