4104ad8f7bab04dcabfa9bbe0f94174b90066c9a
adi-uchiha <adi.cs.night@gmail.com>
2025-05-11 12:51:50 +0530

    Use create automation optimistic UI

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -2,8 +2,12 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import Loader from '../loader'
 import { FastForwardIcon } from 'lucide-react'
+import { useCreateAutomation } from '@/hooks/use-automations'
 
 const CreateAutomation = () => {
+
+	  const {} = useCreateAutomation()
+
 	return (
 		<Button className='lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 test-white rounded-full from-[#3352CC] to-[#1C2D70]'>
 			<Loader state={false}>
